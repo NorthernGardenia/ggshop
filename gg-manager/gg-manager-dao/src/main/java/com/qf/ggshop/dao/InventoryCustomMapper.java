023e297802a950de1205140794e3cc996f9254ec
easts7890 <635793722@qq.com>
2017-11-27 08:52:25 +0800

    库存查询

---
+++ gg-manager/gg-manager-dao/src/main/java/com/qf/ggshop/dao/InventoryCustomMapper.java
@@ -0,0 +1,15 @@
+package com.qf.ggshop.dao;
+
+import com.qf.ggshop.pojo.po.GsOrder;
+import com.qf.ggshop.pojo.po.GsOrderExample;
+import com.qf.ggshop.pojo.po.Inventory;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+public interface InventoryCustomMapper {
+    int countInventory();
+
+    List<Inventory> listInventoryByPage(Map<String,Object> map);
+}
